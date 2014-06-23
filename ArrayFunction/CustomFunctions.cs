32fb685e09f59616874fec88931a7fc759eef5a4
shahidnav <shahid.naveed@mnscorp.net>
2014-06-23 14:50:18 +0100

    Final commit

---
+++ ArrayFunction/CustomFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayFunction
+{
+    public static class CustomFunctions
+    {
+        public static int[] GetShuffledArray()
+        {
+            var shuffledArray = new int[1000];
+            int index = 0;
+
+            while (index < shuffledArray.Length)
+            {
+                shuffledArray[index] = ++index;
+            }
+
+            var randomGenerator = new Random();
+
+            while (index > 1)
+            {
+                int randomIndex = randomGenerator.Next(index);
+                int valueToSwap = shuffledArray[--index];
+                shuffledArray[index] = shuffledArray[randomIndex];
+                shuffledArray[randomIndex] = valueToSwap;
+            }
+
+            return shuffledArray;
+        }
+    }
+}
---
+++ ArrayFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayFunction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] shuffledArray = CustomFunctions.GetShuffledArray();
+            int index = 0;
+
+            while (index < shuffledArray.Length)
+            {
+                Console.WriteLine("Position: {0}, Value: {1}", index, shuffledArray[index++]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
